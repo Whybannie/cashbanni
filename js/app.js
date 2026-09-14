35db7923189a8c080173565d02cd777e8df95527
Антон Маслаков <antoniobandero11@gmail.com>
2026-09-14 23:04:17 +0700

    final: connect to working cloudflare worker

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ function haptic(k){ try{ if(!TG||!TG.HapticFeedback) return;
   else if(k==='error')TG.HapticFeedback.notificationOccurred('error');
   else TG.HapticFeedback.impactOccurred('light'); }catch(e){} }
 
-const API_BASE = "http://5.83.140.208:26018";
+const API_BASE = "https://holy-hill-647b.antoniobandero11.workers.dev";
 async function api(path, opts = {}) {
   try {
     const init = (TG && TG.initData) ? TG.initData : "";
