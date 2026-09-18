3e57de086c41841c11bbbc0596926078810ce3ea
Антон Маслаков <antoniobandero11@gmail.com>
2026-09-18 21:03:51 +0700

    hotfix: working app.js + caseimg override

--- js/app.js
+++ js/app.js
@@ -214,18 +214,9 @@ function openChannel(){ try{ TG&&TG.openTelegramLink?TG.openTelegramLink(CHANNEL
 
 // ---------- КЕЙСЫ (дизайнерские арты + RTP 80%) ----------
 function caseArt(c){
-const CASE_IMG = {
-  free:'case_fri.JPG', fri:'case_fri.JPG',
-  starter:'case_starter.JPG', start:'case_starter.JPG',
-  mini:'case_mini.JPG',
-  xaip:'case_xaip.JPG', hype:'case_xaip.JPG', haip:'case_xaip.JPG',
-  premium:'case_premium.JPG', prem:'case_premium.JPG'
-};
-function caseArt(c){
   const col=CASE_COLORS[c.rarity];
-  const src=c.img||CASE_IMG[c.id];
-  const art = src
-    ? '<img class="case-img" src="'+src+'" alt="'+c.name+'">'
+  const art = c.img
+    ? '<img class="case-img" src="'+c.img+'" alt="'+c.name+'" loading="lazy">'
     : '<div class="bow"></div><div class="lid"></div><div class="body"></div><div class="rv"></div><div class="rh"></div><div class="em">'+c.em+'</div>';
   return '<div class="case-art" style="--c0:'+col[0]+';--c1:'+col[1]+';--c2:'+col[2]+';--glow:'+RAR[c.rarity].glow+'">'+art+'</div>';
 }
